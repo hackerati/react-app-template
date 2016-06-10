e5119461e647ab8c4299071df4cfe6a163d9fd5c
Geoff Scott <geoff@hackerati.us>
2016-06-10 18:36:27 -0400

    fix action.types bug

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -2,14 +2,20 @@
 
 import { Map } from 'immutable'
 
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../constants/ActionTypes';
+import { INCREMENT_COUNTER, DECREMENT_COUNTER, INCREMENT_COUNTER_IF_ODD } from '../constants/ActionTypes';
 
 export default function counter (state, action) {
-    switch (action) {
+    switch (action.type) {
     case INCREMENT_COUNTER:
         return (state.set ('value', state.get ('value') + 1))
     case DECREMENT_COUNTER:
         return (state.set ('value', state.get ('value') - 1))
+    case INCREMENT_COUNTER_IF_ODD:
+        const current_value = state.get ('value')
+
+        if (current_value % 2 === 0) { return (state) }
+
+        return (state.set ('value', current_value + 1))
     default:
         return (Map ({ value: 0 }))
     }
--- tests/reducers/counter_spec.js
+++ tests/reducers/counter_spec.js
@@ -4,7 +4,7 @@ import {expect} from 'chai'
 import {Map, fromJS} from 'immutable'
 
 import counter from '../../src/reducers/counter'
-import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../../src/constants/ActionTypes';
+import { INCREMENT_COUNTER, DECREMENT_COUNTER, INCREMENT_COUNTER_IF_ODD } from '../../src/constants/ActionTypes';
 
 describe ('counter reducer', () => {
     it ('should handle initial state', () => {
@@ -14,19 +14,35 @@ describe ('counter reducer', () => {
         }))
     })
 
-    it ('should handle INCREMENT_COUNTER', () => {
+    it ('should increment on INCREMENT_COUNTER', () => {
         const state = Map ({ value : 0 })
-        const new_state = counter (state, INCREMENT_COUNTER)
+        const new_state = counter (state, { type: INCREMENT_COUNTER })
         expect (new_state).to.equal (fromJS ({
             value: 1
         }))
     })
 
-    it ('should handle DECREMENT_COUNTER', () => {
+    it ('should decrement on DECREMENT_COUNTER', () => {
         const state = Map ({ value : 0 })
-        const new_state = counter (state, DECREMENT_COUNTER)
+        const new_state = counter (state, { type: DECREMENT_COUNTER })
         expect (new_state).to.equal (fromJS ({
             value: -1
+        }))
+    })
+
+    it ('should increment on INCREMENT_COUNTER_IF_ODD when counter is odd', () => {
+        const state = Map ({ value : 1 })
+        const new_state = counter (state, { type: INCREMENT_COUNTER_IF_ODD })
+        expect (new_state).to.equal (fromJS ({
+            value: 2
+        }))
+    })
+
+    it ('should not increment on INCREMENT_COUNTER_IF_ODD when counter is even', () => {
+        const state = Map ({ value : 2 })
+        const new_state = counter (state, { type: INCREMENT_COUNTER_IF_ODD })
+        expect (new_state).to.equal (fromJS ({
+            value: 2
         }))
     })
 })
