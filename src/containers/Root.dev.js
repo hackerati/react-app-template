60476e08cd98f673b1fe1d8d0760440a248eaef8
Geoff Scott <geoff@hackerati.us>
2016-06-14 16:08:43 -0400

    moved CounterApp container into counter directory

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import CounterApp from './CounterApp'
+import CounterApp from '../counter/CounterApp'
 import DevTools from './DevTools'
 
 export default class Root extends Component {
