8fda96b609f5c8eb094da2dbe48ebeef0895d006
Geoff Scott <geoff@hackerati.us>
2016-06-15 23:45:56 -0400

    refactor actions export

--- src/counter/index.js
+++ src/counter/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import * as actions from './actions'
 import reducer from './reducer'
-export default { actions, reducer }
+export default { reducer }
+export * from './actions'
 export { default as CounterApp } from './components/CounterApp'
