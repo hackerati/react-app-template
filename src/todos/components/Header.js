c60bbc11886ff335141387aafcd9d732a9856cec
Geoff Scott <geoff@hackerati.us>
2016-06-23 20:47:56 -0400

    Header should render correctly

--- src/todos/components/Header.js
+++ src/todos/components/Header.js
@@ -1,8 +1,17 @@
 'use strict'
 
 import React, { PropTypes, Component } from 'react'
+import TodoTextInput from './TodoTextInput'
 
 class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>todos</h1>
+        <TodoTextInput newTodo placeholder="What needs to be done?" />
+      </header>
+    )
+  }
 }
 
 export default Header
--- tests/todos/components/Header.spec.js
+++ tests/todos/components/Header.spec.js
@@ -5,24 +5,34 @@ import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import sinon from 'sinon'
 import Header from '../../../src/todos/components/Header'
+import TodoTextInput from '../../../src/todos/components/TodoTextInput'
 
 function setup () {
-  const props = {
-    addTodo: sinon.spy (),
-  }
-
   const component = shallow (
-    <Header {...props} />
+    <Header/>
   )
 
   return {
-    props: props,
     component: component,
   }
 }
 
 describe ('Header component', () => {
-    it ('should render correctly')
+    it ('should render correctly', () => {
+        const { component } = setup ()
 
-    it ('should call addTodo if length of text is greater than 0')
+        expect(component.node.type).to.equal('header')
+        expect(component.node.props.className).to.equal('header')
+
+        const [ h1, input ] = component.node.props.children
+
+        expect(h1.type).to.equal('h1')
+        expect(h1.props.children).to.equal('todos')
+
+        expect(input.type).to.equal(TodoTextInput)
+        expect(input.props.newTodo).to.equal(true)
+        expect(input.props.placeholder).to.equal('What needs to be done?')
+    })
+
+  it ('should call addTodo if length of text is greater than 0')
 })
