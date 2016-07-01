d0d3ad477fd0ab54365c7f3989750d4be800fbfc
Geoff Scott <geoff@hackerati.us>
2016-07-01 08:23:10 -0400

    edit action

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -5,3 +5,8 @@ import * as types from './ActionTypes'
 export function add (text) {
     return { type: types.ADD, description: text, completed: false }
 }
+
+export function edit (id, text) {
+    console.log (id + ' ' + text)
+    return { type: types.EDIT, id: id, description: text }
+}
