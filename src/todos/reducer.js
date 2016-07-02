25779bc3f10e251cde95a43be386c9d22123d947
Geoff Scott <geoff@hackerati.us>
2016-07-01 23:49:17 -0400

    implemented EDIT, DELETE, and COMPLETE reducers

--- src/todos/reducer.js
+++ src/todos/reducer.js
@@ -1,15 +1,26 @@
 'use strict'
 
 import { List, Map } from 'immutable'
+import uuid from 'uuid'
 import * as types from './ActionTypes'
 
 export default function reducer (state = List ([]), action) {
     switch (action.type) {
     case types.ADD:
         return (state.push (Map ({
+           id: uuid.v4(),
 	   description: action.description,
 	   completed: false,
 	})))
+    case types.DELETE:
+        return (state.filter ( todo => todo.get('id') !== action.id ))
+    case types.EDIT:
+        return (state.map (todo => todo.get('id') === action.id ?
+                           todo.set ('description', action.description) : todo))
+    case types.COMPLETE:
+        return (state.map (todo => todo.get('id') === action.id ?
+                           todo.set ('completed', !todo.get('completed')) : todo
+        ))
     default:
         // just return the same state
         return (state)
--- tests/todos/reducer.spec.js
+++ tests/todos/reducer.spec.js
@@ -1,7 +1,8 @@
 'use strict'
 
 import { expect } from 'chai'
-import { List, fromJS } from 'immutable'
+import { List, Map, fromJS } from 'immutable'
+import uuid from 'uuid'
 
 import todos from '../../src/todos'
 
@@ -16,11 +17,77 @@ describe ('Todos reducer', () => {
         const new_state = todos.reducer (state, { type: todos.types.ADD,
                                                   description: 'My task',
                                                   completed: false })
-        expect (new_state).to.equal (fromJS ([
+        expect(new_state.first().filterNot((v, k) => k === 'id')).to.equal(fromJS ( // skip id field
             {
                 description: 'My task',
                 completed: false
             }
-        ]))
+        ))
+    })
+
+    it ('should handle EDIT task', () => {
+        const state = List ([
+          Map ({
+            id: uuid.v4(),
+	    description: 'My task',
+	    completed: false
+          }),
+          Map ({
+            id: uuid.v4(),
+	    description: 'My next task',
+	    completed: false
+          })
+        ])
+        const new_state = todos.reducer (state, { type: todos.types.EDIT,
+                                                  id: state.get(1).get('id'),
+                                                  description: 'My updated task' })
+        expect (new_state.size).to.equal(2)
+        expect (new_state.get(0)).to.equal(state.get(0))
+        expect (new_state.get(1).get('id')).to.equal(state.get(1).get('id'))
+        expect (new_state.get(1).get('description')).to.equal('My updated task')
+        expect (new_state.get(1).get('completed')).to.equal(state.get(1).get('completed'))
+    })
+
+    it ('should handle DELETE task', () => {
+        const state = List ([
+          Map ({
+            id: uuid.v4(),
+	    description: 'My task',
+	    completed: false
+          }),
+          Map ({
+            id: uuid.v4(),
+	    description: 'My next task',
+	    completed: false
+          })
+        ])
+        const new_state = todos.reducer (state, { type: todos.types.DELETE, id: state.get(1).get('id')})
+        expect (new_state.size).to.equal(1)
+        expect (new_state.get(0)).to.equal(state.get(0))
+    })
+
+    it ('should handle COMPLETE task', () => {
+        const state = List ([
+          Map ({
+            id: uuid.v4(),
+	    description: 'My task',
+	    completed: false
+          }),
+          Map ({
+            id: uuid.v4(),
+	    description: 'My next task',
+	    completed: false
+          })
+        ])
+        const new_state = todos.reducer (state, { type: todos.types.COMPLETE,
+                                                  id: state.get(1).get('id') })
+        expect (new_state.size).to.equal(2)
+        expect (new_state.get(0)).to.equal(state.get(0))
+        expect (new_state.get(1).get('id')).to.equal(state.get(1).get('id'))
+        expect (new_state.get(1).get('description')).to.equal(state.get(1).get('description'))
+        expect (new_state.get(1).get('completed')).to.equal(!state.get(1).get('completed'))
+        const new_state1 = todos.reducer (new_state, { type: todos.types.COMPLETE,
+                                                       id: state.get(1).get('id') })
+        expect (new_state1.get(1).get('completed')).to.equal(state.get(1).get('completed'))
     })
 })
