654dffdb49149e03bd723f0207f924160c893695
Nenad Samardzic <samardzicnenad@gmail.com>
2017-08-04 17:39:49 -0400

    include a list of todos

--- src/todomvc/components/MainSection.js
+++ src/todomvc/components/MainSection.js
@@ -1,11 +1,24 @@
 'use strict';
 
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+import TodoItem from './TodoItem'
 
 export default class MainSection extends Component {
+  static propTypes = {
+    todos: PropTypes.object.isRequired
+  };
+
   render() {
+    const props = this.props;
+
     return (
-      <section/>
+      <section>
+        <ul>
+          {props.todos.map(todo => <TodoItem key={todo.get('id')} todo={todo}/>)}
+        </ul>
+      </section>
     )
   }
 }
--- tests/todomvc/components/MainSection.spec.js
+++ tests/todomvc/components/MainSection.spec.js
@@ -1,18 +1,29 @@
 'use strict';
 
 import React from 'react'
+import {List, Map} from 'immutable'
+import uuid from 'uuid'
 import {expect} from 'chai'
 import {shallow} from 'enzyme'
 
 import MainSection from '../../../src/todomvc/components/MainSection'
+import TodoItem from '../../../src/todomvc/components/TodoItem'
 
 function setup() {
+  const props = {
+    todos: List([
+      Map({id: uuid.v4(), description: 'Use Redux', completed: false}),
+      Map({id: uuid.v4(), description: 'Run the tests', completed: false})
+    ])
+  };
+
   const component = shallow(
-    <MainSection/>
+    <MainSection {...props} />
   );
 
   return {
-    component: component
+    component: component,
+    props: props
   }
 }
 
@@ -22,6 +33,19 @@ describe('MainSection component', () => {
       const {component} = setup();
 
       expect(component.type()).to.equal('section')
+    });
+
+    it('Should include a list of todos', () => {
+      const {component, props} = setup();
+      const ul = component.children('ul');
+      const items = ul.children();
+
+      expect(ul).to.have.length(1);
+      expect(items).to.have.length(props.todos.size);
+      items.forEach((item, i) => {
+        expect(item.type()).to.equal(TodoItem);
+        expect(item.props().todo).to.equal(props.todos.get(i))
+      })
     })
   })
 });
