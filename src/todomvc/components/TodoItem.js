48ba2af4570578e27f95462fddafce19dfcf2c6b
Nenad Samardzic <samardzicnenad@gmail.com>
2017-08-05 00:07:29 -0400

    switch to edit mode when label onDoubleClick is fired

--- src/todomvc/components/TodoItem.js
+++ src/todomvc/components/TodoItem.js
@@ -2,21 +2,47 @@
 
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import TodoTextInput from './TodoTextInput'
 
 export default class TodoItem extends Component {
   static propTypes = {
     todo: PropTypes.object.isRequired
   };
 
-  render() {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      editing: false
+    }
+  }
+
+  handleDoubleClick() {
+    this.setState({editing: true})
+  }
+
+  renderTodoListItem() {
     const {todo} = this.props;
 
     return (
       <li>
-        <label>
+        <label onDoubleClick={ this.handleDoubleClick.bind(this) }>
           { todo.get('description') }
         </label>
       </li>
     )
+  }
+
+  renderTodoTextInput() {
+    const {todo} = this.props;
+
+    return (
+      <li>
+        <TodoTextInput text={ todo.get('description') }/>
+      </li>
+    )
+  }
+
+  render() {
+    return ( this.state.editing ? this.renderTodoTextInput() : this.renderTodoListItem() )
   }
 }
--- tests/todomvc/components/TodoItem.spec.js
+++ tests/todomvc/components/TodoItem.spec.js
@@ -37,5 +37,18 @@ describe('TodoItem component', () => {
       expect(label).to.have.length(1);
       expect(label.children().text()).to.equal(props.todo.get('description'))
     })
+  });
+
+  describe('Should behave correctly', () => {
+    it('Should switch to edit mode when label onDoubleClick is fired', () => {
+      const {component} = setup();
+
+      component.children('label').simulate('doubleclick'); // switch to edit mode
+
+      const input = component.find('TodoTextInput');
+
+      expect(input).to.have.length(1);
+      expect(input.prop('text')).to.equal('Use Redux')
+    })
   })
 });
